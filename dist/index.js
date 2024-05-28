51aca341f521016c6662ecd8f58ab732c51bbd9a
Jongita <jurgitajonaityte@gmail.com>
2024-05-28 21:30:31 +0300

    employee table

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mysql2_1 = __importDefault(require("mysql2"));
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 let connected = false;
 const con = mysql2_1.default.createConnection({
     host: "localhost",
@@ -23,27 +24,27 @@ const server = http_1.default.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     console.log(url === null || url === void 0 ? void 0 : url.split("/"));
-    //     let filePath = `public${url}`;
-    // if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()){
-    //     console.log(path.extname(filePath));
-    //     const ext=path.extname(filePath);
-    //     switch (ext) {
-    //         case ".css":
-    //             res.setHeader("Content-Type", "text/css; charset=utf-8");
-    //             break;
-    //         case ".js":
-    //             res.setHeader("Content-Type", "application/javascript; charset=utf-8");
-    //             break;
-    //         case ".jpg":
-    //         case ".png":
-    //         case ".jpeg":
-    //             res.setHeader("Content-Type", "image/jpg; charset=utf-8");
-    //             break;
-    //     }
-    //     let file=fs.readFileSync(filePath);
-    //     res.write(file);
-    //     return res.end();
-    // }
+    let filePath = `public${url}`;
+    if (fs_1.default.existsSync(filePath) && fs_1.default.lstatSync(filePath).isFile()) {
+        console.log(path_1.default.extname(filePath));
+        const ext = path_1.default.extname(filePath);
+        switch (ext) {
+            case ".css":
+                res.setHeader("Content-Type", "text/css; charset=utf-8");
+                break;
+            case ".js":
+                res.setHeader("Content-Type", "application/javascript; charset=utf-8");
+                break;
+            case ".jpg":
+            case ".png":
+            case ".jpeg":
+                res.setHeader("Content-Type", "image/jpg; charset=utf-8");
+                break;
+        }
+        let file = fs_1.default.readFileSync(filePath);
+        res.write(file);
+        return res.end();
+    }
     if (url == '/employees' && method == 'GET') {
         if (connected) {
             con.query("SELECT * FROM employees;", (error, result) => {
@@ -53,7 +54,7 @@ const server = http_1.default.createServer((req, res) => {
                 let rows = "";
                 result.forEach((e) => {
                     rows += "<tr>";
-                    rows += `<td>${e.name}</td> <td>${e.surname} </td> <td>${e.education}</td> <td>${e.salary}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows += `<td>${e.name}</td> <td>${e.surname} </td> <td>${e.education != null ? e.education : '-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
                     rows += "</tr>";
                 });
                 let template = fs_1.default.readFileSync('templates/employees.html').toString();
@@ -69,8 +70,16 @@ const server = http_1.default.createServer((req, res) => {
             if (error)
                 throw error;
             let employee = result[0];
+            console.log(employee);
             res.setHeader("Contect-Type", "text/html; charset=utf-8");
             let template = fs_1.default.readFileSync('templates/employee.html').toString();
+            template = template.replace("{{ name }}", employee.name);
+            template = template.replace("{{ surname }}", employee.surname);
+            template = template.replace("{{ gender}}", employee.gender != null ? employee.gender : '-');
+            template = template.replace("{{ phone }}", employee.phone != null ? employee.phone : '-');
+            template = template.replace("{{ birthday }}", employee.birthday != null ? employee.birthday.toLocaleDateString() : '-');
+            template = template.replace("{{ education }}", employee.education != null ? employee.education : '-');
+            template = template.replace("{{ salary }}", employee.salary != null ? (employee.salary / 100).toString() : '-');
             res.write(template);
             res.end();
         });
--- src/index.ts
+++ src/index.ts
@@ -30,28 +30,28 @@ con.connect((error:any)=>{
 
         console.log(url?.split("/"));
 
-    //     let filePath = `public${url}`;
+        let filePath = `public${url}`;
     
-    // if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()){
-    //     console.log(path.extname(filePath));
-    //     const ext=path.extname(filePath);
-    //     switch (ext) {
-    //         case ".css":
-    //             res.setHeader("Content-Type", "text/css; charset=utf-8");
-    //             break;
-    //         case ".js":
-    //             res.setHeader("Content-Type", "application/javascript; charset=utf-8");
-    //             break;
-    //         case ".jpg":
-    //         case ".png":
-    //         case ".jpeg":
-    //             res.setHeader("Content-Type", "image/jpg; charset=utf-8");
-    //             break;
-    //     }
-    //     let file=fs.readFileSync(filePath);
-    //     res.write(file);
-    //     return res.end();
-    // }
+    if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()){
+        console.log(path.extname(filePath));
+        const ext=path.extname(filePath);
+        switch (ext) {
+            case ".css":
+                res.setHeader("Content-Type", "text/css; charset=utf-8");
+                break;
+            case ".js":
+                res.setHeader("Content-Type", "application/javascript; charset=utf-8");
+                break;
+            case ".jpg":
+            case ".png":
+            case ".jpeg":
+                res.setHeader("Content-Type", "image/jpg; charset=utf-8");
+                break;
+        }
+        let file=fs.readFileSync(filePath);
+        res.write(file);
+        return res.end();
+    }
 
 
         if(url=='/employees' && method=='GET'){
@@ -62,7 +62,7 @@ con.connect((error:any)=>{
         let rows="";
         result.forEach((e)=>{
             rows+="<tr>";
-            rows+=`<td>${e.name}</td> <td>${e.surname} </td> <td>${e.education}</td> <td>${e.salary}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
+            rows+=`<td>${e.name}</td> <td>${e.surname} </td> <td>${e.education != null? e.education:'-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
             rows+="</tr>";
 
         })
@@ -79,10 +79,18 @@ con.connect((error:any)=>{
             con.query<Employee[]>(`SELECT * FROM employees WHERE id=${id};`, (error, result) => {
         if(error) throw error;
         let employee=result[0];
+        console.log(employee);
         res.setHeader("Contect-Type", "text/html; charset=utf-8");
         
         let template=fs.readFileSync('templates/employee.html').toString();
-        
+       
+        template=template.replace("{{ name }}", employee.name);
+        template=template.replace("{{ surname }}", employee.surname);
+        template=template.replace("{{ gender}}", employee.gender!=null?employee.gender:'-');
+        template=template.replace("{{ phone }}", employee.phone!=null?employee.phone:'-');
+        template=template.replace("{{ birthday }}", employee.birthday!=null?employee.birthday.toLocaleDateString():'-');
+        template=template.replace("{{ education }}", employee.education!=null?employee.education:'-');
+        template=template.replace("{{ salary }}", employee.salary!=null?(employee.salary / 100).toString():'-');
         res.write(template);
         res.end();
     });
