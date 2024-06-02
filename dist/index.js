a4256e710cef49100f0bcc853a726195ffbc837c
Jongita <jurgitajonaityte@gmail.com>
2024-06-02 22:52:22 +0300

    add_remove

--- dist/index.js
+++ dist/index.js
@@ -50,15 +50,15 @@ const server = http_1.default.createServer((req, res) => {
             con.query("SELECT * FROM employees;", (error, result) => {
                 if (error)
                     throw error;
-                res.setHeader("Contect-Type", "text/html; charset=utf-8");
+                res.setHeader("Content-Type", "text/html; charset=utf-8");
                 let rows = "";
                 result.forEach((e) => {
                     rows += "<tr>";
-                    rows += `<td>${e.id}</td> <td>${e.name}</td> <td>${e.surname} </td> <td>${e.education != null ? e.education : '-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows += `<td>${e.id}</td> <td>${e.name}</td> <td>${e.surname}</td> <td>${e.education != null ? e.education : '-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a> <a href='/delete/${e.id}' class="btn btn-danger">Ištrinti</a></td>`;
                     rows += "</tr>";
                 });
                 let template = fs_1.default.readFileSync('templates/employees.html').toString();
-                template = template.replace(`{{employees_table}}`, rows);
+                template = template.replace('{{employees_table}}', rows);
                 res.write(template);
                 res.end();
             });
@@ -81,6 +81,55 @@ const server = http_1.default.createServer((req, res) => {
             template = template.replace("{{ education }}", employee.education != null ? employee.education : '-');
             template = template.replace("{{ salary }}", employee.salary != null ? (employee.salary / 100).toString() : '-');
             res.write(template);
+            res.end();
+        });
+    }
+    if (url == '/add' && method == 'POST') {
+        if (connected) {
+            const reqBody = [];
+            req.on('data', (d) => {
+                reqBody.push(d);
+            });
+            req.on('end', () => {
+                const reqData = decodeURIComponent(Buffer.concat(reqBody).toString());
+                const dd = reqData.split('&');
+                console.log(dd);
+                const name = mysql2_1.default.escape(dd[0].split('=')[1]);
+                const surname = mysql2_1.default.escape(dd[1].split('=')[1]);
+                const gender = mysql2_1.default.escape(dd[2].split('=')[1]);
+                const phone = mysql2_1.default.escape(dd[3].split('=')[1]);
+                const birthday = mysql2_1.default.escape(dd[4].split('=')[1]);
+                const education = mysql2_1.default.escape(dd[5].split('=')[1]);
+                const salary = mysql2_1.default.escape(dd[6].split('=')[1]);
+                const sql = `INSERT INTO employees(name, surname, gender, phone, birthday, education, salary) VALUES (${name}, ${surname}, ${gender}, ${phone}, ${birthday}, ${education}, ${salary})`;
+                con.query(sql, (error) => {
+                    if (error)
+                        throw error;
+                });
+                res.writeHead(302, {
+                    'Location': '/employees'
+                });
+                res.end();
+            });
+        }
+    }
+    //Funkcija iškviečiame kai ateiname į pridėjimo langą
+    if (url == '/add' && method == 'GET') {
+        if (connected) {
+            let template = fs_1.default.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
+        }
+    }
+    if ((url === null || url === void 0 ? void 0 : url.split("/")[1]) == 'delete') {
+        //Pasiimame iš url id
+        let id = parseInt(url === null || url === void 0 ? void 0 : url.split("/")[2]);
+        con.query(`DELETE FROM employees WHERE id=${id};`, (error, result) => {
+            if (error)
+                throw error;
+            res.writeHead(302, {
+                'Location': '/employees'
+            });
             res.end();
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -53,24 +53,26 @@ con.connect((error:any)=>{
         return res.end();
     }
 
-
-        if(url=='/employees' && method=='GET'){
+     
+    if (url=='/employees' && method=='GET'){
         if (connected){
-        con.query<Employee[]>("SELECT * FROM employees;", (error, result) => {
-        if(error) throw error;
-        res.setHeader("Contect-Type", "text/html; charset=utf-8");
-        let rows="";
-        result.forEach((e)=>{
-            rows+="<tr>";
-            rows+=`<td>${e.id}</td> <td>${e.name}</td> <td>${e.surname} </td> <td>${e.education != null? e.education:'-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a></td>`;
-            rows+="</tr>";
-
-        })
-        let template=fs.readFileSync('templates/employees.html').toString();
-        template=template.replace(`{{employees_table}}`, rows);
-        res.write(template);
-        res.end();
-    });
+            con.query<Employee[]>("SELECT * FROM employees;", (error,result)=>{
+                if (error) throw error;
+                res.setHeader("Content-Type", "text/html; charset=utf-8");
+                let rows="";
+                result.forEach((e)=>{ 
+                    rows+="<tr>";
+                    rows+=`<td>${e.id}</td> <td>${e.name}</td> <td>${e.surname}</td> <td>${e.education != null? e.education:'-'}</td> <td>${e.salary != null ? (e.salary / 100).toFixed(2) : '-'}</td> <td> <a href='/employee/${e.id}' class="btn btn-success">Plačiau</a> <a href='/delete/${e.id}' class="btn btn-danger">Ištrinti</a></td>`;
+                    rows+="</tr>";
+                });
+               
+                let template=fs.readFileSync('templates/employees.html').toString();
+                template=template.replace('{{employees_table}}', rows);
+                
+              
+                res.write(template);        
+                res.end();
+            });   
         }
     }
 
@@ -94,6 +96,66 @@ con.connect((error:any)=>{
         res.write(template);
         res.end();
     });
+
+    }
+      
+    if (url=='/add' && method=='POST'){
+        if (connected){
+           const reqBody:any[]=[];
+           req.on('data', (d)=>{
+                reqBody.push(d);
+           });
+           req.on('end', ()=>{
+            
+                const reqData=decodeURIComponent(Buffer.concat(reqBody).toString());
+                const dd=reqData.split('&');
+                console.log(dd); 
+                
+                const name= mysql.escape(dd[0].split('=')[1]);
+                const surname=mysql.escape(dd[1].split('=')[1]);
+                const gender=mysql.escape(dd[2].split('=')[1]);
+                const phone=mysql.escape(dd[3].split('=')[1]);
+                const birthday=mysql.escape(dd[4].split('=')[1]);
+                const education=mysql.escape(dd[5].split('=')[1]);
+                const salary=mysql.escape(dd[6].split('=')[1]);
+
+                const sql=`INSERT INTO employees(name, surname, gender, phone, birthday, education, salary) VALUES (${name}, ${surname}, ${gender}, ${phone}, ${birthday}, ${education}, ${salary})`;
+                con.query(sql, (error)=>{
+                    if (error) throw error;
+                });
+
+                res.writeHead(302, {
+                    'Location':'/employees'
+                })
+                res.end();
+           });
+
+
+        } 
+    }
+
+      //Funkcija iškviečiame kai ateiname į pridėjimo langą
+    if (url=='/add' && method=='GET'){
+        if (connected){
+            let template=fs.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
+        } 
+    }
+
+    
+    if ( url?.split("/")[1] == 'delete' ){
+        //Pasiimame iš url id
+        let id=parseInt(url?.split("/")[2]);
+        con.query<Employee[]>(`DELETE FROM employees WHERE id=${id};`, (error,result)=>{
+            if (error) throw error;
+            res.writeHead(302, {
+                'Location':'/employees'
+            })
+            res.end();
+        });
+        
+        
     }
 
 
